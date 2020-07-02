ab470430e63d9fc94cf263ac57bcbdb4ecc1866a
buivinhhieu217 <buivinhhieu217@gmail.com>
2020-07-02 19:49:44 +0700

    Add project files.

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CNWeb_BTL_BanGoiTapTheHinh
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CNWeb_BTL_BanGoiTapTheHinh.MvcApplication" Language="C#" %>
